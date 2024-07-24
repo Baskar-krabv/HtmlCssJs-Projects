fe65cc4ad63d36b0da301b57156b367568780bc2
Baskar-krabv <baskarkrab02@gmail.com>
2024-07-24 19:22:52 +0530

    upload

--- admin-script.js
+++ admin-script.js
@@ -3,15 +3,26 @@ var c1=document.getElementById("choice1");
 var c2=document.getElementById("choice2");
 var c3=document.getElementById("choice3");
 var c4=document.getElementById("choice4");
+i=0;
+var cont=0;
+localStorage.setItem("count",cont);
 document.getElementById("btn__upload").addEventListener("click",()=>{
     var question=qus.value;
     var ch1=c1.value;
     var ch2=c2.value;
     var ch3=c3.value;
     var ch4=c4.value;
-    localStorage.setItem("question",question);
-    localStorage.setItem("choice1",ch1)
-    localStorage.setItem("choice2",ch2)
-    localStorage.setItem("choice3",ch3)
-    localStorage.setItem("choice4",ch4)
+    cont+=1;
+    localStorage.setItem("question'"+i+"'",question);
+    localStorage.setItem("choice1'"+i+"'",ch1);
+    localStorage.setItem("choice2'"+i+"'",ch2);
+    localStorage.setItem("choice3'"+i+"'",ch3);
+    localStorage.setItem("choice4'"+i+"'",ch4);
+    i+=1;
+    qus.value="";
+    c1.value="";
+    c2.value="";
+    c3.value="";
+    c4.value="";
 });
+
--- index-script.js
+++ index-script.js
@@ -1,21 +1,24 @@
 var btnSave=document.getElementById("btn_save");
-btnSave.addEventListener("click",()=>{
-    var newdiv=document.createElement("div");
-    h2=document.createElement("h2");
-    h3__1=document.createElement("h3");
-    h3__2=document.createElement("h3");
-    h3__3=document.createElement("h3");
-    h3__4=document.createElement("h3");
-    newdiv.classList.add("questions");
-    document.querySelector(".question__main").appendChild(newdiv);
-    newdiv.appendChild(h2);
-    newdiv.appendChild(h3__1);
-    newdiv.appendChild(h3__2);
-    newdiv.appendChild(h3__3);
-    newdiv.appendChild(h3__4);
-    h2.textContent=localStorage.getItem("question");
-    h3__1.textContent=localStorage.getItem("choice1");
-    h3__2.textContent=localStorage.getItem("choice2");
-    h3__3.textContent=localStorage.getItem("choice3");
-    h3__4.textContent=localStorage.getItem("choice4");
-});
+var main=document.querySelector(".question__main");
+var newDiv=document.querySelector(".questions");
+var count=localStorage.getItem("count");
+for(i=0;i<count;i++)
+{
+    var h2=document.createElement("h2");
+    var h3__1=document.createElement("h3");
+    var h3__2=document.createElement("h3");
+    var h3__3=document.createElement("h3");
+    var h3__4=document.createElement("h3");
+    newDiv.appendChild(h2);
+    newDiv.appendChild(h3__1);
+    newDiv.appendChild(h3__2);
+    newDiv.appendChild(h3__3);
+    newDiv.appendChild(h3__4);
+    h2.textContent=localStorage.getItem("question'"+i+"'");
+    h3__1.textContent=localStorage.getItem("choice1'"+i+"'");
+    h3__2.textContent=localStorage.getItem("choice2'"+i+"'");
+    h3__3.textContent=localStorage.getItem("choice3'"+i+"'");
+    h3__4.textContent=localStorage.getItem("choice4'"+i+"'");
+    i+=1;
+}
+    
